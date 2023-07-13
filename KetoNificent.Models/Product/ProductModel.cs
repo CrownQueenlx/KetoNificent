20e34f1630e29415ff1e804e416012974a950a3a
Brianne W <brianne4god@gmail.com>
2023-07-13 13:57:34 -0400

    Fixed productModel

--- KetoNificent.Models/Product/ProductModel.cs
+++ KetoNificent.Models/Product/ProductModel.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using KetoNificent.Data.Entities;
+using KetoNificent.Models.User;
 
 namespace KetoNificent.Models.Product;
 
@@ -9,8 +9,8 @@ public class ProductModel
     [Key]
     public int Id { get; set; }
 
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty; //the name of the final product
 
-    [ForeignKey(nameof(Id))]
+    [ForeignKey(nameof(UserDetail.ID))]
     public int UserId { get; set; }
 }
---
+++ KetoNificent.Models/User/UserDetail.cs
@@ -0,0 +1,10 @@
+namespace KetoNificent.Models.User;
+
+public class UserDetail
+{
+    public int ID { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    
+}
