67363eaf76862fab4d14af57d5ba9a7cd51775fd
Brianne W <brianne4god@gmail.com>
2023-07-17 10:54:35 -0400

    Created SRP models for User

--- KetoNificent.Models/User/UserDetail.cs
+++ KetoNificent.Models/User/UserDetail.cs
@@ -3,6 +3,7 @@ namespace KetoNificent.Models.User;
 public class UserDetail
 {
     public int UserId { get; set; }
+    public string Username { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
